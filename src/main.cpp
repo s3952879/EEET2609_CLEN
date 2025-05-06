05131a82b9b51a1437ea87ae61b16b6f23f33c7b
OscarEwen <s3897420@student.rmit.edu.au>
2025-05-06 16:58:50 +1000

    Hello world reached - Project now has a basic webpage which it outputs - Webserver successfully runs on ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Network credentials
+const char *ssid = "SSID_HERE";
+const char *password = "PASSWORD_HERE";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Setup web server port number
+AsyncWebServer server(80);
+
+// String to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup()
+{
+    Serial.begin(115200);
+
+     // Initialize SPIFFS
+    if(!SPIFFS.begin(true)){
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/index.html", String(), false);
+    });
+    
+    // Route to load style.css file
+    server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/style.css", "text/css");
+    });
+
+    server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    
 }
