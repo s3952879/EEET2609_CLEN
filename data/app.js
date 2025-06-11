d5c4206271bd4876b6c5473d56bd8d654a7a0faa
OscarEwen <s3897420@student.rmit.edu.au>
2025-06-11 17:31:14 +1000

    Mockup changed to be adaptive - Websocket code added, disabled for purposes of using code for mockup - Variable number of states added. - Variable state selector added.

---
+++ data/app.js
@@ -0,0 +1,98 @@
+// For websocket connection, disabled to design mockup
+
+/*let gateway = `ws://${window.location.hostname}/ws`;
+let websocket;
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+function onOpen(event) {
+    console.log('Connection opened');
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+function onMessage(event) {
+    var state;
+    if (event.data == "1") {
+        state = "ON";
+    }
+    else {
+        state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+}
+
+//window.addEventListener('load', onLoad);
+
+function onLoad(event) {
+    initWebSocket();
+}*/
+
+const numStateInput = document.getElementById("number-states-input");
+const numStateBtn = document.getElementById("number-states-submit")
+const stateSelect = document.getElementById("state-select");
+
+let prevNumberStates;
+let numberStates = 0;
+
+document.getElementById("test").innerHTML;
+
+if (numStateInput.value) {
+    while (numberStates < numStateInput.value) {
+        ++numberStates;
+        let newState = document.createElement("option");
+        newState.innerHTML = `State ${numberStates}`;
+        newState.setAttribute("id", `state-${numberStates}`);
+        newState.setAttribute("name", `state-${numberStates}`);
+        stateSelect.appendChild(newState);
+    }
+}
+
+
+let onNumStateChange = () => {
+    // New amount of states is greater than original value
+    if (numberStates > stateSelect.childElementCount) {
+        while (numberStates > stateSelect.childElementCount) {
+            let newState = document.createElement("option");
+            newState.innerHTML = `State ${stateSelect.childElementCount+1}`;
+            newState.setAttribute(
+                "id", 
+                `state-${stateSelect.childElementCount+1}`
+            );
+            newState.setAttribute(
+                "name", 
+                `state-${stateSelect.childElementCount+1}`
+            );
+            stateSelect.appendChild(newState);
+        }
+    }
+    else if (numberStates < stateSelect.childElementCount) {
+        while (numberStates < stateSelect.childElementCount) {
+            let lastState = document.getElementById(`state-${stateSelect.childElementCount}`);
+            stateSelect.removeChild(lastState);
+        } 
+    }
+};
+
+numStateBtn.onclick = () => {
+    if (numStateInput.value
+        && numStateInput.value >= 1
+        && numStateInput.value <= 64) {
+        prevNumberStates = numberStates;
+        numberStates = numStateInput.value;
+        onNumStateChange();
+    }
+    else {
+        numStateInput.value = numberStates;
+    }
+
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include "AsyncTCP.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
+// Baudrate
+const int baudrate = 115200;
+
 // Network credentials
-const char *ssid = "SSID_HERE";
-const char *password = "PASSWORD_HERE";
+const char *ssid = "Karoewen";
+const char *password = "Jasper11";
 
 // Setup web server port number
 AsyncWebServer server(80);
+
+AsyncWebSocket ws("/ws");
 
 // String to store the HTTP request
 String header;
@@ -16,16 +22,44 @@ String header;
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
+void onEvent(AsyncWebSocket *server,
+             AsyncWebSocketClient *client,
+             AwsEventType type,
+             void *arg,
+             uint8_t *data,
+             size_t len)
+{
+    switch (type)
+    {
+    case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+    case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+    case WS_EVT_DATA:
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+        break;
+    }
+}
+
+void initWebsocket()
+{
+    server.addHandler(&ws);
+}
+
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(baudrate);
 
-     // Initialize SPIFFS
-    if(!SPIFFS.begin(true)){
+    // Initialize SPIFFS
+    if (!SPIFFS.begin(true))
+    {
         Serial.println("An Error has occurred while mounting SPIFFS");
         return;
     }
@@ -46,20 +80,24 @@ void setup()
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
+    initWebsocket();
+
     // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/index.html", String(), false);
-    });
-    
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/index.html", String(), false); });
+
     // Route to load style.css file
-    server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/style.css", "text/css");
-    });
+    server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/style.css", "text/css"); });
+
+    // Route to load app.js file
+    server.on("/app.js", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/app.js", "text/javascript"); });
 
     server.begin();
 }
 
 void loop()
 {
-    
+    ws.cleanupClients();
 }
